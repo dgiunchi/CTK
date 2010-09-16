cdce25745ae4f858c5c5c6d683902708803f1174
Daniele Giunchi <d.giunchi@scsolutions.it>
2010-09-16 13:07:11 +0200

    modify adapter class for working with mafEventBus

--- Plugins/org.commontk.eventbus/ctkEventBusImpl.cpp
+++ Plugins/org.commontk.eventbus/ctkEventBusImpl.cpp
@@ -1,9 +1,11 @@
 #include "ctkEventBusImpl_p.h"
 
 #include <QSetIterator>
-
 #include "ctkEventHandlerWrapper_p.h"
 
+
+//using namespace mafCore;
+using namespace mafEventBus;
 
 ctkEventBusImpl* ctkEventBusImpl::instance()
 {
@@ -26,29 +28,42 @@ void ctkEventBusImpl::sendEvent(const ctkEvent& event)
   dispatchEvent(event, false);
 }
 
-void ctkEventBusImpl::publishSignal(const QObject* publisher, const char* signal)
+void ctkEventBusImpl::publishSignal(const QObject* publisher, const char* signal, const char *signal_topic)
 {
-  Q_UNUSED(publisher)
-  Q_UNUSED(signal)
+    // event bus addproperty Event
+    //need a TOPIC
+    mafEvent *event = new mafEvent(signal_topic, mafEventTypeLocal, mafSignatureTypeSignal, const_cast<QObject *>(publisher), signal);
+    m_MafEventBusManager->addEventProperty(*event);
 }
 
 void ctkEventBusImpl::subscribeSlot(const QObject* subscriber, const char* member, const Properties& properties)
 {
+    QStringList topicList;
+    QVariant v = properties[EventConstants::EVENT_TOPIC];
+    topicList = v.toStringList();
+
+    QStringListIterator i(topicList);
+    while (i.hasNext()) {
+        mafEvent *mesbEvent = new mafEvent(i.next(), mafEventTypeLocal, mafSignatureTypeCallback, const_cast<QObject *>(subscriber), member);
+        m_MafEventBusManager->addEventProperty(*mesbEvent);
+    }
 
   // TODO check for duplicates
 
-  ctkEventHandlerWrapper* wrapper = new ctkEventHandlerWrapper(subscriber, member, properties);
+  /*ctkEventHandlerWrapper* wrapper = new ctkEventHandlerWrapper(subscriber, member, properties);
   if (wrapper->init())
   {
     bucket(wrapper);
-  }
+  }*/
 }
 
 void ctkEventBusImpl::dispatchEvent(const ctkEvent& event, bool isAsync)
 {
   Q_UNUSED(isAsync)
 
-  QString topic = event.topic();
+
+
+  /*QString topic = event.topic();
 
   QSet<ctkEventHandlerWrapper*> eventHandlers = this->handlers(topic);
   if (eventHandlers.empty()) return;
@@ -57,7 +72,17 @@ void ctkEventBusImpl::dispatchEvent(const ctkEvent& event, bool isAsync)
   while (iter.hasNext())
   {
     iter.next()->handleEvent(event);
-  }
+  }*/
+
+  QString topic = event.topic(); //may contains widlcards
+
+  mafEvent *mebEvent = new mafEvent();
+  mebEvent->setEventTopic(topic);
+  mebEvent->setEventType(mafEventTypeLocal);
+  mebEvent->setEventFilter(NULL);
+  //need to think about the arguments inside the event
+  m_MafEventBusManager->notifyEvent(*mebEvent);
+
 }
 
 void ctkEventBusImpl::bucket(ctkEventHandlerWrapper* wrapper)
