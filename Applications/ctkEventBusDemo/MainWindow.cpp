b8731c03396a89bbec1c8514271d61dff65bba9f
elpablo <paolo.quadrani@gmail.com>
2010-09-17 13:06:30 +0200

    Demo UI class containing the stuff for event message sending

---
+++ Applications/ctkEventBusDemo/MainWindow.cpp
@@ -0,0 +1,30 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::sendEvent() {
+    qDebug("Sending event...");
+}
+
+void MainWindow::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
